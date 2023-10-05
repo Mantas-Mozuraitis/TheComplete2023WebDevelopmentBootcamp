548e77f4d416ce9a4ef0fdb2236996e2fd57d7a8
Mantas Mozuraitis <mozmantas@gmail.com>
2023-10-05 20:52:29 +0100

    adding key press functionality

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,36 +1,43 @@
+document.addEventListener("keydown", function(event){
+    checkKey(event.key);
+});
+
 document.querySelectorAll(".drum").forEach((drum)=>{
     drum.addEventListener("click", function(){
         var letterPress = drum.textContent;
-
-        switch (letterPress) {
-            case 'w':
-                playSound("./sounds/crash.mp3");
-                break;
-            case 'a':
-                playSound("./sounds/kick-bass.mp3");
-                break;
-            case 's':
-                playSound("./sounds/snare.mp3");
-                break;
-            case 'd':
-                playSound("./sounds/tom-1.mp3");
-                break;
-            case 'j':
-                playSound("./sounds/tom-2.mp3");
-                break;
-            case 'k':
-                playSound("./sounds/tom-3.mp3");
-                break;
-            case 'l':
-                playSound("./sounds/tom-4.mp3");
-                break;
-            
-            default:
-                console.log(letterPress);
-                break;
-        }
+        checkKey(letterPress);
     })
 })
+
+function checkKey(key){
+    switch (key) {
+        case 'w':
+            playSound("./sounds/crash.mp3");
+            break;
+        case 'a':
+            playSound("./sounds/kick-bass.mp3");
+            break;
+        case 's':
+            playSound("./sounds/snare.mp3");
+            break;
+        case 'd':
+            playSound("./sounds/tom-1.mp3");
+            break;
+        case 'j':
+            playSound("./sounds/tom-2.mp3");
+            break;
+        case 'k':
+            playSound("./sounds/tom-3.mp3");
+            break;
+        case 'l':
+            playSound("./sounds/tom-4.mp3");
+            break;
+        
+        default:
+            console.log(letterPress);
+            break;
+    }
+}
 
 function playSound(filePath){
     var sound = new Audio(filePath);
