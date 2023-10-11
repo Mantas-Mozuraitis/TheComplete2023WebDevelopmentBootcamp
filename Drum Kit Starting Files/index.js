25acdff8a1a73a1b3597a10f31d9366f51a11f11
Mantas Mozuraitis <mozmantas@gmail.com>
2023-10-11 12:05:18 +0100

    added animation for key press

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -12,25 +12,25 @@ document.querySelectorAll(".drum").forEach((drum)=>{
 function checkKey(key){
     switch (key) {
         case 'w':
-            playSound("./sounds/crash.mp3");
+            playSound("./sounds/crash.mp3", key);
             break;
         case 'a':
-            playSound("./sounds/kick-bass.mp3");
+            playSound("./sounds/kick-bass.mp3", key);
             break;
         case 's':
-            playSound("./sounds/snare.mp3");
+            playSound("./sounds/snare.mp3", key);
             break;
         case 'd':
-            playSound("./sounds/tom-1.mp3");
+            playSound("./sounds/tom-1.mp3", key);
             break;
         case 'j':
-            playSound("./sounds/tom-2.mp3");
+            playSound("./sounds/tom-2.mp3", key);
             break;
         case 'k':
-            playSound("./sounds/tom-3.mp3");
+            playSound("./sounds/tom-3.mp3", key);
             break;
         case 'l':
-            playSound("./sounds/tom-4.mp3");
+            playSound("./sounds/tom-4.mp3", key);
             break;
         
         default:
@@ -39,7 +39,16 @@ function checkKey(key){
     }
 }
 
-function playSound(filePath){
+function playSound(filePath,key){
     var sound = new Audio(filePath);
     sound.play();
+    buttonAnimation(key);
+}
+
+function buttonAnimation(keyPressed){
+    var activeBtn = document.querySelector("."+keyPressed);
+    activeBtn.classList.toggle("pressed");
+    setTimeout(function(){
+        activeBtn.classList.toggle("pressed");
+    },100);
 }
