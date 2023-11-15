7da58ed285a6aa771ee82ac9775f463d9f418491
Mantas Mozuraitis <mozmantas@gmail.com>
2023-11-15 12:28:48 +0000

    EJS exercise update

--- EJS/index.js
+++ EJS/index.js
@@ -4,9 +4,19 @@ const port = 3000;
 
 app.get("/", (req, res) =>{
     const currentDate = new Date();
-    var dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const currentDay = currentDate.getDay();
+    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let type = dayNames[currentDay];
+    let adv;
+    if(type ==="Saturday" || type === "Sunday"){
+        adv = "it's time to party hard!"
+    }else{
+        adv = "it's time to work hard!"
+    }
     res.render("index.ejs",{
-        dayOfWeek:dayNames[currentDate.getDay()-1]
+        dayType: type,
+        advice: adv,
     })
 })
 
