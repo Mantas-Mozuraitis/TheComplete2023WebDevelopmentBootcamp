ffcb30392c70ea5410dc644e77be210903cd1a26
Mantas Mozuraitis <mozmantas@gmail.com>
2023-11-30 11:42:03 +0000

    completion of exercise

--- 5.3 Axios/index.js
+++ 5.3 Axios/index.js
@@ -8,9 +8,6 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Step 1: Make sure that when a user visits the home page,
-//   it shows a random activity.You will need to check the format of the
-//   JSON data from response.data and edit the index.ejs file accordingly.
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
@@ -25,8 +22,6 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
-
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
   // sure you're passing both the type and participants queries.
@@ -35,6 +30,18 @@ app.post("/", async (req, res) => {
   // Step 3: If you get a 404 error (resource not found) from the API request.
   // Pass an error to the index.ejs to tell the user:
   // "No activities that match your criteria."
+  const obj = req.body;
+  try {
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${obj.type}&participants=${obj.participants}`);
+    const result = response.data[Math.floor(Math.random() * response.data.length)];
+    res.render("index.ejs", { dataspec: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+      errorcode: error.message.slice(-3)
+    });
+  }
 });
 
 app.listen(port, () => {
