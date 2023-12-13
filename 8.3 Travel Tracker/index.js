471d7862794a2e67c558b90e6d1b1681257cb498
Mantas Mozuraitis <mozmantas@gmail.com>
2023-12-13 19:57:48 +0000

    Implement ADD request to add countries using form input

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -19,16 +19,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  const result = await db.query("SELECT * FROM visited_countries");
-  const visited_countries = [];
+  let visited_countries = [];
+  const result = (await db.query("SELECT * FROM visited_countries"));
+
   result.rows.forEach(row => {
     visited_countries.push(row.country_code);
   });
-  console.log(visited_countries);
   res.render("index.ejs", {
     countries: visited_countries,
     total: result.rowCount,
   })
+});
+
+app.post("/add", async (req,res)=>{
+  const new_country = req.body.country;
+  const countries = (await db.query("SELECT * FROM countries")).rows;
+  
+  let index = countries.findIndex((country) => country.country_name.toLowerCase()===new_country.toLowerCase());
+  if (index > -1) {
+    db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [countries[index].country_code]);
+    res.redirect("/");
+  }else{
+    console.error("Could not insert new data");
+  }
 });
 
 app.listen(port, () => {
