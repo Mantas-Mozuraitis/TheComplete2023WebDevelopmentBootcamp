97f15f6c985276e59da152434bfb11e7d5621d67
Mantas Mozuraitis <mozmantas@gmail.com>
2024-01-05 21:20:53 +0000

    Implement new user registration using posgresql

--- Secrets App/app.js
+++ Secrets App/app.js
@@ -1,10 +1,19 @@
 //jshint esversion:6
 import express from "express"
 import bodyParser from "body-parser"
-import ejs from "ejs"
+import pg from "pg"
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "secrets_app",
+    password: "password",
+    port: 5432,
+})
+db.connect();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -19,6 +28,15 @@ app.get("/login", (req,res)=>{
 
 app.get("/register", (req,res)=>{
     res.render("register.ejs");
+})
+app.post("/register", async (req,res)=>{
+    try {
+        await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [req.body.username, req.body.password]);
+        console.log("new user has been created");
+        res.redirect("/");
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
 })
 
 app.listen(port, (req,res)=>{
