de953736d0179dd8ebe9476d4fd612cfa6fec088
Mantas Mozuraitis <mozmantas@gmail.com>
2024-01-07 13:49:27 +0000

    Implement database encryption for user passwords

--- Secrets App/app.js
+++ Secrets App/app.js
@@ -14,6 +14,7 @@ const db = new pg.Client({
     port: 5432,
 })
 db.connect();
+const secret = "ThisIsMyLittleSecret!..%$5edkf"
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -28,11 +29,13 @@ app.get("/login", (req,res)=>{
 })
 app.post("/login", async (req,res)=>{
     try {
-        const user = await db.query("SELECT * FROM users WHERE email = $1", [req.body.username]);
+        const user = await db.query("SELECT id, email, pgp_sym_decrypt(password, $1) as decrypted_password FROM users WHERE email = $2", [secret, req.body.username]);
         if (user.rowCount>0) {
-            user.rows[0].password === req.body.password
-            ?res.render("secrets.ejs")
-            :console.log("Password is incorrect"), res.redirect("/login");
+            if (user.rows[0].decrypted_password === req.body.password) {
+                res.render("secrets.ejs")
+            }else{
+                console.log("Password is incorrect"), res.redirect("/login");
+            }
         } else{
             console.log("user does not exist");
             res.redirect("/login");
@@ -49,7 +52,7 @@ app.get("/register", (req,res)=>{
 })
 app.post("/register", async (req,res)=>{
     try {
-        await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [req.body.username, req.body.password]);
+        await db.query("INSERT INTO users (email, password) VALUES ($1, pgp_sym_encrypt($2,$3,'cipher-algo=aes256'))", [req.body.username, req.body.password, secret]);
         console.log("new user has been created");
         res.render("secrets.ejs");
     } catch (error) {
@@ -60,3 +63,4 @@ app.post("/register", async (req,res)=>{
 app.listen(port, (req,res)=>{
     console.log(`Server is listening on port ${port}`);
 })
+
